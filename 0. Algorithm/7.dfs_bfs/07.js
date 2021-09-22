484471d66e3a2070cb99a19c785bbd0946b3aa2e
jkpark104 <jkrang104@gmail.com>
2021-09-22 19:15:27 +0900

    feat: TIL
    1. bfs & dfs review ; Q.07

--- 0. Algorithm/7.dfs_bfs/07.js
+++ 0. Algorithm/7.dfs_bfs/07.js
@@ -1,4 +1,40 @@
-function solution(board) {}
+function solution(board) {
+  const q = [];
+  const n = board.length;
+  const m = board[0].length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (board[i][j] === 1) {
+        q.push([i, j]);
+      }
+    }
+  }
+
+  const dx = [1, 0, -1, 0];
+  const dy = [0, 1, 0, -1];
+  while (q.length) {
+    const [x, y] = q.shift();
+
+    for (let i = 0; i < 4; i++) {
+      const [nx, ny] = [x + dx[i], y + dy[i]];
+      if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] === 0) {
+        board[nx][ny] = board[x][y] + 1;
+        q.push([nx, ny]);
+      }
+    }
+  }
+
+  let ans = 0;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (board[i][j] === 0) return -1;
+      ans = Math.max(ans, board[i][j]);
+    }
+  }
+  return ans - 1;
+}
 
 console.log(
   solution([
@@ -7,46 +43,4 @@ console.log(
     [0, 0, -1, 0, 0, 0],
     [0, 0, 0, 0, -1, 1]
   ])
-);  
-
-// function solution(board) {
-//   function bfs() {
-//     const dx = [1, 0, -1, 0]
-//     const dy = [0, 1, 0, -1]
-
-//     const q = []
-//     for (let i = 0; i < n; i++) {
-//       for (let j = 0; j < m; j++) {
-//         if (board[i][j] === 1) q.push([i, j])
-//       }
-//     }
-
-//     while (q.length) {
-//       const [x, y] = q.shift()
-
-//       for (let i = 0; i < 4; i++) {
-//         const [nx, ny] = [x + dx[i], y + dy[i]]
-
-//         if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-//           if (board[nx][ny] === 0) {
-//             board[nx][ny] = 1
-//             chk[nx][ny] = chk[x][y] + 1
-//             ans = chk[nx][ny]
-//             q.push([nx, ny])
-//           }
-//         }
-//       }
-//     }
-//   }
-//   const n = board.length
-//   const m = board[0].length
-//   const chk = new Array(n).fill().map(() => new Array(m).fill(0))
-//   let ans = undefined
-//   bfs()
-//   for (let i = 0; i < n; i++) {
-//     for (let j = 0; j < m; j++) {
-//       if (board[i][j] === 0) return -1
-//     }
-//   }
-//   return ans
-// }
+);
